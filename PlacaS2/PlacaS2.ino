6198e11f629e1940d0159a0a27a6c6c71dfcd9f7
gabriel henrique koch <gabril_h_koch@estudante.sesisenai.org.br>
2025-11-26 09:56:02 -0300

    Código placa S2 concluído

--- PlacaS2/PlacaS2.ino
+++ PlacaS2/PlacaS2.ino
@@ -1,21 +1,43 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include "env.h"
 
-WiFiClient client;
+WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-const String BrokerURL = "test.mosquitto.org";
-const int BrokerPort = 1883;
-const String BrokerUser = "";
-const String BrokerPass = "";
+// Definição dos pinos - sensor 1
+const byte TRIGGER_PIN1 = 2;
+const byte ECHO_PIN1 = 4;
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+// Definição dos pinos - sensor 2  
+const byte TRIGGER_PIN2 = 5;
+const byte ECHO_PIN2 = 18;
+
+const int ledPin = 19;
+
+// Tópicos MQTT
+const char* TOPIC_PRESENCA1 = "sensor/ultrassonico1";
+const char* TOPIC_PRESENCA2 = "sensor/ultrassonico2";
+const char* TOPIC_ILUMINACAO = "iluminacao";
 
 void setup() {
   Serial.begin(115200);
+  client.setInsecure();
+  
+  // Configuração sensor 1
+  pinMode(TRIGGER_PIN1, OUTPUT);
+  pinMode(ECHO_PIN1, INPUT);
+  
+  // Configuração sensor 2
+  pinMode(TRIGGER_PIN2, OUTPUT);
+  pinMode(ECHO_PIN2, INPUT);
+  
+  // Configuração LED
+  pinMode(ledPin, OUTPUT);
+
   Serial.println("Conectando ao WiFi");
-  WiFi.begin(SSID,PASS);
+  WiFi.begin(SSID, PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
@@ -23,29 +45,83 @@ void setup() {
   Serial.println("\nConectado com Sucesso!");
 
   Serial.println("Conectando ao Broker...");
-  mqtt.setServer(BrokerURL.c_str(), BrokerPort);
+  
+  mqtt.setServer(BrokerURL, BrokerPort);
+  mqtt.setCallback(callback);
+  
   String BoardID = "S2";
   BoardID += String(random(0xffff), HEX);
-  mqtt.connect(BrokerURL.c_str(), BrokerUser.c_str(), BrokerPass.c_str());
-  while(!mqtt.connected()) {
-    Serial.print(".");
-    delay(200);
+  
+  if(mqtt.connect(BoardID.c_str(), BrokerUser, BrokerPass)) {
+    Serial.println("\nConectado ao Broker!");
+    mqtt.subscribe(TOPIC_ILUMINACAO);
+  } else {
+    Serial.println("\nFalha ao conectar ao Broker!");
+    Serial.print("Estado: ");
+    Serial.println(mqtt.state());
   }
-  Serial.println("\nConectado ao Broker!");
+}
+
+// Função para ler distância
+long lerDistancia(byte triggerPin, byte echoPin) {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  
+  long duracao = pulseIn(echoPin, HIGH);
+  long distancia = duracao * 0.034924 / 2;
+  
+  return distancia;
 }
 
 void loop() {
-  mqtt.publish("Iluminação")
+  // Sensor 1
+  long distancia1 = lerDistancia(TRIGGER_PIN1, ECHO_PIN1);
+  
+  Serial.print("Sensor 1 - Distância: ");
+  Serial.print(distancia1);
+  Serial.println(" cm");
 
+  if (distancia1 < 10) {
+    Serial.println("Trem detectado no sensor 1!");
+    mqtt.publish(TOPIC_PRESENCA1, "Trem detectado");
+    delay(2000);
+  }
+
+  // Sensor 2
+  long distancia2 = lerDistancia(TRIGGER_PIN2, ECHO_PIN2);
+  
+  Serial.print("Sensor 2 - Distância: ");
+  Serial.print(distancia2);
+  Serial.println(" cm");
+
+  if (distancia2 < 10) {
+    Serial.println("Trem detectado no sensor 2!");
+    mqtt.publish(TOPIC_PRESENCA2, "Trem detectado");
+    delay(2000);
+  }
+
+  mqtt.loop();
+  delay(500);
 }
-void callback(char* topic, byte* payload, unsigned int lenght){
-String msg = "";
-for(int i = 0; i < length; i++){
-  msg += (char) payload[i];
-}
-if(topic == "Iluminação" && msg == "Acender"){
-  digitalWrite(2,high);
-}else if(topic == "Iluminação" && msg == "Apagar"){
-  digitalWrite(2,low)
-}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String msg = "";
+  for(int i = 0; i < length; i++) {
+    msg += (char) payload[i];
+  }
+  
+  Serial.println("Mensagem recebida: " + msg + " no tópico: " + String(topic));
+  
+  if(strcmp(topic, TOPIC_ILUMINACAO) == 0) {
+    if(msg.equals("Acender")) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED aceso");
+    } else if(msg.equals("Apagar")) {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED apagado");
+    }
+  }
 }
